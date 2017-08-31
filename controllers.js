bf40eddf43e5c23ea47195ceb97569b43e87cf86
Zihao Zhao <zhaozihao7199@gmail.com>
2017-08-30 18:01:30 -0700

    update api key

--- controllers.js
+++ controllers.js
@@ -1,3 +1,4 @@
+
 // CONTROLLERS
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
     
@@ -12,23 +13,32 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
-    
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=7e180e4edb2eb7b5d57c279041d3e3f8", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=7e180e4edb2eb7b5d57c279041d3e3f8", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
+    
     $scope.convertToFahrenheit = function(degK) {
-        
+        console.log("origin degree " + degK);
         return Math.round((1.8 * (degK - 273)) + 32);
         
     }
     
     $scope.convertToDate = function(dt) { 
-      
-        return new Date(dt * 1000);
+        
+        return new Date(dt);
         
     };
+
     
+//    console.log($scope.weatherResult);
+//    console.log($scope.weatherResult.$promise.then(function(obj){
+//        console.log(obj.list.length);
+//        for (let i = 0; i < obj.list.length; i++) {
+//            console.log(obj.list[i]);
+//        }
+//        
+//    }));
 }]);
